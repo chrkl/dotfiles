10a2284bc6c5f569bf165f8ea16ff8e5a79859de
Christoph Kleineweber <christoph@quobyte.com>
2018-11-21 16:56:09 +0100

    Fixed install.sh for existing links

--- install.sh
+++ install.sh
@@ -12,16 +12,16 @@ git submodule init && git submodule update
 mkdir -p $HOME/.vimbackup
 mkdir -p $HOME/.config/nvim
 
-ln -s $HOME/dotfiles/.vim/ $HOME/
-ln -s $HOME/dotfiles/.vimrc $HOME/
-ln -s $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
-ln -s $HOME/dotfiles/.vim/bundle $HOME/config/nvim/
-ln -s $HOME/dotfiles/.vim/colors $HOME/config/nvim/
-ln -s $HOME/dotfiles/.zsh/ $HOME/
-ln -s $HOME/dotfiles/.zshrc $HOME/
-ln -s $HOME/dotfiles/.zlogin $HOME/
-ln -s $HOME/dotfiles/.tmux.conf $HOME/
-ln -s $HOME/dotfiles/.fzf/ $HOME/
+ln -sf $HOME/dotfiles/.vim/ $HOME/
+ln -sf $HOME/dotfiles/.vimrc $HOME/
+ln -sf $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
+ln -sf $HOME/dotfiles/.vim/bundle $HOME/.config/nvim/
+ln -sf $HOME/dotfiles/.vim/colors $HOME/.config/nvim/
+ln -sf $HOME/dotfiles/.zsh/ $HOME/
+ln -sf $HOME/dotfiles/.zshrc $HOME/
+ln -sf $HOME/dotfiles/.zlogin $HOME/
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/
+ln -sf $HOME/dotfiles/.fzf/ $HOME/
 
 $HOME/.fzf/install --all
 
