b32b3380cf7cf93af2381df8f3cb192ca229ac97
Christoph Kleineweber <christoph@kleineweber.de>
2018-11-23 23:29:02 +0100

    Fixed fzf linking

--- install.sh
+++ install.sh
@@ -21,9 +21,7 @@ ln -sf $HOME/dotfiles/.zsh/ $HOME/
 ln -sf $HOME/dotfiles/.zshrc $HOME/
 ln -sf $HOME/dotfiles/.zlogin $HOME/
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/
-if [ -f $HOME/.fzf ]; then
-    ln -s $HOME/dotfiles/.fzf/ $HOME/
-fi
+ln -sf $HOME/dotfiles/.fzf/ $HOME/
 
 $HOME/.fzf/install --all
 
