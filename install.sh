124ce05ff7ba687f73ad721af7d0d1df8d17db20
Christoph Kleineweber <christoph@kleineweber.de>
2026-09-12 12:48:41 +0200

    Add bat

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ ln -sf $HOME/dotfiles/.fzf/ $HOME/
 ln -sf $HOME/dotfiles/.functions $HOME/
 ln -sf $HOME/dotfiles/.dir_colors $HOME/
 ln -sf $HOME/dotfiles/.config/nvim $HOME/.config/
+ln -sf $HOME/dotfiles/.config/bat $HOME/.config/
 
 $HOME/.fzf/install --all
 
