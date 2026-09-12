7c9356b12979c8b1c20140472bc972007868d77c
Christoph Kleineweber <christoph@kleineweber.de>
2026-09-12 12:51:50 +0200

    Add helix

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ ln -sf $HOME/dotfiles/.dir_colors $HOME/
 ln -sf $HOME/dotfiles/.config/nvim $HOME/.config/
 ln -sf $HOME/dotfiles/.config/bat $HOME/.config/
 ln -sf $HOME/dotfiles/.config/hunk $HOME/.config/
+ln -sf $HOME/dotfiles/.config/helix $HOME/.config/
 
 $HOME/.fzf/install --all
 
